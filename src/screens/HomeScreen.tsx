55f23097e0e18c9e9c40af1cd33f073938f0b910
Hannah <hannahtjacob@gmail.com>
2026-07-19 15:08:08 -0400

    create separate pages

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View } from 'react-native';
+
+export function HomeScreen() {
+  return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#081c15',
+    flex: 1,
+  },
+});
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View } from 'react-native';
+
+export function ProfileScreen() {
+  return <View style={styles.container} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#081c15',
+    flex: 1,
+  },
+});
